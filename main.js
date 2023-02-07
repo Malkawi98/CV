0d5a0783cb0d02c7f83f1632b0f33f57d49a6c6e
Hamza Malkawi <malkawi98@yahoo.com>
2023-02-07 23:26:18 +0300

    add projects section

--- main.js
+++ main.js
@@ -2,19 +2,17 @@ let lastScrollTop; // This Varibale will store the top position
 
 navbar = document.querySelector(".header-container"); // Get The NavBar
 
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll', function () {
   //on every scroll this funtion will be called
 
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   //This line will get the location on scroll
 
-  if(scrollTop > lastScrollTop){ //if it will be greater than the previous
-    navbar.style.top='-120px';
+  if (scrollTop > lastScrollTop) { //if it will be greater than the previous
+    navbar.style.top = '-120px';
     //set the value to the negetive of height of navbar
-  }
-
-  else{
-    navbar.style.top='0';
+  } else {
+    navbar.style.top = '0';
   }
 
   lastScrollTop = scrollTop; //New Position Stored
@@ -38,14 +36,14 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.querySelector(".navbar").addEventListener("click",function () {
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelector(".navbar").addEventListener("click", function () {
     document.querySelector('body').classList.toggle("nav-open");
   });
 
   let navItems = document.querySelectorAll(".nav-item")
   navItems.forEach(function (item) {
-    item.addEventListener("click",function () {
+    item.addEventListener("click", function () {
       document.querySelector('body').classList.toggle("nav-open");
     });
   });
